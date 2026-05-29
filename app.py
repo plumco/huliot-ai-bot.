28281cbf90b4e188ddfee32def4881e1c137567e
Umesh Nikam <50052472+plumco@users.noreply.github.com>
2026-05-29 18:08:10 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from streamlit.components.v1 import html
-
-st.title("Huliot Pipe Inspector AI")
-
-with open("app.html", "r", encoding="utf-8") as f:
-    page_html = f.read()
-
-html(page_html, height=850)
